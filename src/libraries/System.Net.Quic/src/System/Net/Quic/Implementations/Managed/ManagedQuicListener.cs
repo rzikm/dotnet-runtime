18b5ec12f790ca7fdd386601a12423373847a58f
Radek Zikmund <r.zikmund.rz@gmail.com>
2023-09-13 12:59:18 +0200

    Validate connectionOptions

--- src/libraries/System.Net.Quic/src/System/Net/Quic/Implementations/Managed/ManagedQuicListener.cs
+++ src/libraries/System.Net.Quic/src/System/Net/Quic/Implementations/Managed/ManagedQuicListener.cs
@@ -25,6 +25,8 @@ namespace System.Net.Quic.Implementations.Managed
         private ManagedQuicListener(QuicListenerOptions options)
             : base(true)
         {
+            options.Validate(nameof(options));
+
             var listenEndPoint = options.ListenEndPoint ?? new IPEndPoint(IPAddress.Any, 0);
 
             var channel = Channel.CreateBounded<ManagedQuicConnection>(new BoundedChannelOptions(options.ListenBacklog)
